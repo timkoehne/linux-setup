c5c3c46b5891fae9352ed8226cc62385bced1a8a
Tim Köhne <20153452+timkoehne@users.noreply.github.com>
2025-11-16 20:33:23 +0100

    set timeout for notifications

--- set_brightness.sh
+++ set_brightness.sh
@@ -4,5 +4,5 @@ brightnessctl set $1
 
 brightness=$(brightnessctl g | awk '{print int($1 / 255 * 100)}')
 
-dunstify --urgency low --hints string:x-dunst-stack-tag:brightness --hints int:value:$brightness "Set Brightness to $brightness%" --icon=brightness-low
+dunstify --urgency low --hints string:x-dunst-stack-tag:brightness --hints int:value:$brightness "Set Brightness to $brightness%" --icon=brightness-low --timeout 2000
 
--- set_volume.sh
+++ set_volume.sh
@@ -4,5 +4,5 @@ wpctl set-volume @DEFAULT_AUDIO_SINK@ $1
 
 volume=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print $2 * 100}')
 
-dunstify --urgency low --hints string:x-dunst-stack-tag:audio --hints int:value:$volume "Set Volume to $volume%" --icon=audio-volume-high
+dunstify --urgency low --hints string:x-dunst-stack-tag:audio --hints int:value:$volume "Set Volume to $volume%" --icon=audio-volume-high --timeout 2000
 
