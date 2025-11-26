d1f780b4e9f2ef6fadf6fa5efe8db950317d7a3d
Tim Köhne <20153452+timkoehne@users.noreply.github.com>
2025-11-26 17:11:55 +0100

    improve setup script to make paths relative

--- setup_script.sh
+++ setup_script.sh
@@ -1,22 +1,32 @@
+#!/bin/bash
 
-git submodule update --init --recursive
+folder_path=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
-ln -f -s ~/Dropbox/Scripts/linux-setup/.bashrc ~/.bashrc
-ln -f -s ~/Dropbox/Scripts/linux-setup/.profile ~/.profile
+# download submodule folder
+git -C "$folder_path" submodule update --init --recursive
 
-ln -f -s ~/Dropbox/Scripts/linux-setup/dunst ~/.config/dunst
-ln -f -s ~/Dropbox/Scripts/linux-setup/waybar ~/.config/waybar
+# softlink dotfiles
+ln -f -s "$folder_path/.bashrc" ~/.bashrc
+ln -f -s "$folder_path/.profile" ~/.profile
 
-[ -d "~/.config/hypr/" ] && mkdir ~/.config/hypr/
-ln -f -s ~/Dropbox/Scripts/linux-setup/hypr/hyprland.conf ~/.config/hypr/hyprland.conf
-ln -f -s ~/Dropbox/Scripts/linux-setup/hypr/hypridle.conf ~/.config/hypr/hypridle.conf
-ln -f -s ~/Dropbox/Scripts/linux-setup/hypr/hyprlock.conf ~/.config/hypr/hyprlock.conf
-touch ~/Dropbox/Scripts/linux-setup/hypr/monitors.conf
-touch ~/Dropbox/Scripts/linux-setup/hypr/workspaces.conf
+# softlink configs
+mkdir -p ~/.config/dunst
+ln -f -s "$folder_path/dunst/dunstrc" ~/.config/dunst/dunstrc
 
-[ -d "~/.config/Code/" ] && mkdir ~/.config/Code/
-[ -d "~/.config/Code/User/" ] && mkdir ~/.config/Code/User
-ln -f -s ~/Dropbox/Scripts/linux-setup/VSCode/settings.json ~/.config/Code/User/settings.json
-ln -f -s ~/Dropbox/Scripts/linux-setup/VSCode/keybindings.json ~/.config/Code/User/keybindings.json
+mkdir -p ~/.config/waybar
+ln -f -s "$folder_path/waybar" ~/.config/waybar
 
-ln -f -s ~/Dropbox/Scripts/linux-setup/Discord/settings.json ~/.config/discord/settings.json
+mkdir -p ~/.config/hypr
+ln -f -s "$folder_path/hypr/hyprland.conf" ~/.config/hypr/hyprland.conf
+ln -f -s "$folder_path/hypr/hypridle.conf" ~/.config/hypr/hypridle.conf
+ln -f -s "$folder_path/hypr/hyprlock.conf" ~/.config/hypr/hyprlock.conf
+echo "env = LINUX_SETUP,$folder_path" > ~/.config/hypr/envvars
+touch ~/.config/hypr/monitors.conf
+touch ~/.config/hypr/workspaces.conf
+
+mkdir -p ~/.config/Code/User
+ln -f -s "$folder_path/VSCode/settings.json" ~/.config/Code/User/settings.json
+ln -f -s "$folder_path/VSCode/keybindings.json" ~/.config/Code/User/keybindings.json
+
+mkdir -p ~/.config/discord
+ln -f -s "$folder_path/Discord/settings.json" ~/.config/discord/settings.json
